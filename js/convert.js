1431ec447e863a6234d30101a68fea448e2a4c6e
João Gustavo <joao.bispo@uvvnet.com.br>
2020-02-18 10:03:16 -0300

    Update
    Css, Js e HTML FEITOS

--- js/convert.js
+++ js/convert.js
@@ -1,12 +1,8 @@
 function convert() {
     var squad = document.getElementsByClassName("squad")[0]
     var inputs = document.getElementsByTagName("input")
-
-    squad.style.borderRadius =
-        `
-        ${inputs[0].value}px
-        ${inputs[2].value}px
-        ${inputs[3].value}px
-        ${inputs[1].value}px
-        `
+    var radius = `${inputs[0].value}px ${inputs[2].value}px ${inputs[3].value}px ${inputs[1].value}px`
+   
+    squad.style.borderRadius = radius
+    squad.getElementsByTagName("p")[0].innerText = "border-radius: " + radius
 }
